e79d6ff908c203546d14792d5302a9c781d8879d
Ramaruva <ramrj1428@gmail.com>
2021-06-02 07:52:34 +0530

    added boiler plate code for project

---
+++ frontendmasters/src/Context/AuthContxetProvider.jsx
@@ -0,0 +1,7 @@
+export const AuthContextProvider =({children})=>
+{
+    return(
+        <>
+        </>
+    )
+}
---
+++ frontendmasters/src/routes/Routes.jsx
@@ -0,0 +1,12 @@
+import { Route, Switch } from "react-router-dom"
+
+export const Routes =()=>
+{
+    return(
+        <Switch>
+              <Route>
+
+              </Route>
+        </Switch>
+    )
+}
