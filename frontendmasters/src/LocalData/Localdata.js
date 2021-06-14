6f50eddb6902b9646029bf5e7146d1529d05154d
unknown <aroul303@gmail.com>
2021-06-14 11:37:26 +0530

    Learn path

--- frontendmasters/src/LocalData/Localdata.js
+++ frontendmasters/src/LocalData/Localdata.js
@@ -71,24 +71,28 @@ export const footerlink=["Courses","Learn","Teachers","Guides","Pricing","Login"
 
 export const learn =[
    {
-      url:"https://static.thenounproject.com/png/48384-200.png",
+      color:"rgb(198,53,40)",
+      url:"https://cdn.icon-icons.com/icons2/2248/PNG/512/sprout_icon_135160.png",
       title:"Beginner",
       des:"Your Path to Becoming a Career-Ready Web Developer!"
 
    },
    {
+      color:"rgb(198,53,40)",
       url:"https://www.freeiconspng.com/thumbs/airplane-icon-png/transport-airplane-takeoff-icon--android-iconset--icons8-2.png",
       title:"Professional",
       des:"Go From Professional Web Developer to Lead Engineer"
 
    },
    {
+      color:"rgb(198,53,40)",
       url:"https://www.searchpng.com/wp-content/uploads/2019/03/Rocket-icon-715x715.png",
       title:"Expert",
       des:"Mind-Expanding Web Development Techniques to Emerging APIs in the Web Browser"
 
    },
    {
+      color:"rgb(198,53,40)",
       url:"https://cdn4.iconfinder.com/data/icons/android-outline-1/64/robot_read_book-512.png",
       title:"Computer Science",
       des:"Learning Computer Science with JavaScript"
@@ -96,15 +100,84 @@ export const learn =[
    },
    {
       url:"http://cdn.onlinewebfonts.com/svg/img_18734.png",
+      color:"rgb(198,53,40)",
       title:"Fullstack",
       des:"Expand Your Abilities and Master the Server"
 
    },
    {
+      color:"rgb(198,53,40)",
       url:"https://cdn1.iconfinder.com/data/icons/stationery-50/512/Holder_office_table_pen_holder_pencil_stationary_stand-512.png",
       title:"Design to Code",
       des:"Make Your Designs Come to Life Through Code!"
+   
 
+   }
+
+]
+
+export const learnCourse =[
+   {
+      url:"https://png.pngtree.com/svg/20170719/javascript_633988.png",
+      title:"JavaScript",
+      des:"Write Professional, Modern JavaScript",
+      color:"yellow"
+
+   },
+   {
+      url:"https://icons.iconarchive.com/icons/martz90/hex/512/css-3-icon.png",
+      title:"CSS",
+      des:"Learn CSS, from Laying Out Websites to Performant Animations",
+      color:"rgb(39,140,199)"
+
+   },
+   {
+      url:"https://www.freepnglogos.com/uploads/javascript-png/javascript-nodejs-logo-27.png",
+      title:"Node Js",
+      des:"Build Web APIs and Applications with Node.js",
+      color:"rgb(0,135,58)"
+
+   },
+   {
+      url:"https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png",
+      title:"React Js",
+      des:"Be Productive with React.js, Today's Most Popular Framework",
+      color:"rgb(0,134,151)"
+   },
+   {
+      url:"https://i.imgur.com/XHVmK6T.png",
+      title:"Vue.js",
+      des:"Learn Vue.js, An Easy to Pick Up and Powerful Framework",
+      color:"rgb(0,187,131)"
+      
+   },
+   {
+      url:"https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png",
+      title:"Angular.js",
+      des:"Learn Angular, a TypeScript-based web application framework led by Google",
+      color:"rgb(255,0,47)"
+
+   },
+   {
+      url:"https://cdn.iconscout.com/icon/free/png-512/webpack-1-1174980.png",
+      title:"Webpack",
+      des:"Learn Webpack, the most popular and flexible build tool for JavaScript",
+      color:"rgb(23,74,128)"
+      
+   },
+   {
+      url:"https://raw.githubusercontent.com/d3/d3-logo/master/d3.png",
+      title:"Data Visualization with D3.js",
+      des:"Turn Data into Visualizations with D3.js",
+      color:"rgb(170,63,63)"
+      
+   },
+   {
+      url:"https://iconape.com/wp-content/files/fm/91325/svg/rxjs-1.svg",
+      title:"Async JS & Rx.js",
+      des:"Learn Asynchronous JavaScript and Rx.js Observables",
+      color:"rgb(210,24,142)"
+      
    }
 
 ]
--- frontendmasters/src/components/Learn/Section.jsx
+++ frontendmasters/src/components/Learn/Section.jsx
@@ -4,18 +4,19 @@ import './LearnPath.css'
 import Pprogress from './Pprogress';
 
 
-function Section({url,des,title}) {
+function Section({url,des,title,color}) {
     return (
         <div >
       <div className="contain">
           <div className="background" style={{backgroundImage:`url(${url})`}}>
-          <Pprogress/>
+         
           </div>
+          <Pprogress/>
           <br />
           <h2>{title}</h2>
-          <p>{des}</p>
+          <p style={{height:"70px"}}>{des}</p>
           <br />
-        <Link to=""> <button className="btn" > start</button></Link> 
+        <Link to=""> <button className="btn" style={{backgroundColor:`${color}`}} > Start</button></Link> 
          </div> 
         </div>
     )
