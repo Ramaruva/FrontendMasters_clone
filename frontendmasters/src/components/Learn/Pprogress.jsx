0fc67885aa043586e6ee2e2551500abe1d066cea
unknown <aroul303@gmail.com>
2021-06-17 04:43:51 +0530

    payment part

--- frontendmasters/src/components/Learn/Pprogress.jsx
+++ frontendmasters/src/components/Learn/Pprogress.jsx
@@ -26,8 +26,6 @@ function Pprogress() {
 							<CircularProgressbar
 								value={value}
 								text={`${roundedValue}%`}
-								/* This is important to include, because if you're fully managing the
-        animation yourself, you'll want to disable the CSS animation. */
 								styles={buildStyles({
 									pathTransition: "none",
 									pathColor: "rgb(213,74,38)",
